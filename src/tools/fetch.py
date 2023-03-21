72dbf130e0587b1d1fc6d8fb5ad38c3630595492
B Burks <33607825+bburks@users.noreply.github.com>
2023-03-21 17:07:48 -0500

    reordered methods so public methods come first

--- src/tools/fetch.py
+++ src/tools/fetch.py
@@ -5,8 +5,37 @@ import json
 import requests
 from tools import paths
 
-SITE = "https://hanabi.live/"
+SITE = "https://hanabi.live"
 ROWS = 100  # note the API cannot exceed size of 100
+
+def fetch_games(username: str):
+    endpoint = f'{SITE}/api/v1/history-full/{username}'
+    response = requests.get(endpoint, timeout=15).text
+    path = paths.get_user_data_path(username)
+    _write_json(response, path)
+
+def fetch_games2(username: str):
+    endpoint = f'{SITE}/api/v1/history/{username}?size={ROWS}'
+    path = paths.get_user_data_path(username)
+    _fetch_paginated(endpoint, path)
+
+def fetch_game(game_id: str):
+    endpoint = f'{SITE}/export/{game_id}'
+    response = requests.get(endpoint, timeout=15).text
+    path = paths.get_game_data_path(game_id)
+    _write_json(response, path)
+
+def fetch_seed(seed: str):
+    endpoint = f'{SITE}/api/v1/seed-full/{seed}'
+    response = requests.get(endpoint, timeout=15).text
+    if response == '[]':
+        print('Server provided no seed data!')
+        return False
+    path = paths.get_seed_data_path(seed)
+    _write_json(response, path)
+    return True
+
+
 
 def _fetch_paginated(url: str, write_to: str, user_specified_limit=1000000):
     """Uses the paginated API to download JSON data. Mainly intended for
@@ -30,33 +59,6 @@ def _fetch_paginated(url: str, write_to: str, user_specified_limit=1000000):
         result.extend(new_result)
 
     _write_json2(result, write_to)
-
-def fetch_games(username: str):
-    endpoint = f'https://hanabi.live/api/v1/history-full/{username}'
-    response = requests.get(endpoint, timeout=15).text
-    path = paths.get_user_data_path(username)
-    _write_json(response, path)
-
-def fetch_games2(username: str):
-    endpoint = f'{SITE}api/v1/history/{username}?size={ROWS}'
-    path = paths.get_user_data_path(username)
-    _fetch_paginated(endpoint, path)
-
-def fetch_game(game_id: str):
-    endpoint = f'https://hanabi.live/export/{game_id}'
-    response = requests.get(endpoint, timeout=15).text
-    path = paths.get_game_data_path(game_id)
-    _write_json(response, path)
-
-def fetch_seed(seed: str):
-    endpoint = f'https://hanabi.live/api/v1/seed-full/{seed}'
-    response = requests.get(endpoint, timeout=15).text
-    if response == '[]':
-        print('Server provided no seed data!')
-        return False
-    path = paths.get_seed_data_path(seed)
-    _write_json(response, path)
-    return True
 
 def _write_json(txt, path):
     with open(path, 'w', encoding="utf8") as outfile:
