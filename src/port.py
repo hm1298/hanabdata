a97dce6bed0a20dce25dca9cb8af71b72b4fd3d8
hm1298 <31015794+hm1298@users.noreply.github.com>
2023-03-29 19:38:21 -0400

    correcting last commit

--- src/port.py
+++
@@ -1,6 +0,0 @@
-from tools.io.update import port_games
-#from tools import update, read
-
-if __name__ == '__main__':
-    port_games()
-    
--- src/update_seed.py
+++
@@ -1,7 +0,0 @@
-"""Prompts user for seed and updates."""
-
-from tools.io.update import update_seed
-
-if __name__ == '__main__':
-    seed = input('enter seed to update: ')
-    update_seed(seed)
