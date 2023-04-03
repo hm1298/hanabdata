22f081dfda36c91680d22bd529ecd8941dc00299
B Burks <33607825+bburks@users.noreply.github.com>
2023-04-02 22:45:24 -0500

    removed always failing test

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,10 +6,6 @@ def test_always_passes():
     """Always passes."""
     assert True
 
-def test_always_fails():
-    """Always fails."""
-    assert False
-
 def test_update_user_does_not_error():
     """Always true. Any errors?"""
     update_user("hallmark")
