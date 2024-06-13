5ad5aa34b4f5663f15c4e35075139715a0e73298
hm1298 <31015794+hm1298@users.noreply.github.com>
2024-06-13 15:21:50 -0400

    introduced FullGamesIterator to structures

---
+++ scripts/find_similar_players.py
@@ -0,0 +1,15 @@
+"""
+This script finds players who take similar actions in identical
+gamestates.
+"""
+
+def main():
+    """script execution"""
+    # go through all games and create a dictionary of seed names to list of data
+    # sort each list by game actions
+    # go through all seeds and create a double dictionary of players to players to count
+    # count the total number of games played, the number of matching gamestates, the number of matching gamestates with similar action chosen (clue<->clue, play<->play), the number of matching gamestates with identical action chosen, the number of matching gamestates with any clue chosen, and the number of matching gamestates with identical clue chosen
+    return
+
+if __name__ == "__main__":
+    main()
